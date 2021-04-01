f0c38462327d5aae7f7c124239a46bc17956ceee
hamsithanamburi <hamsithanamburi@gmail.com>
2021-04-01 21:46:03 +0530

    first commit

---
+++ TweetApp/Entities/Tweet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TweetApp.Entities
+{
+    public partial class Tweet
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string Tweeets { get; set; }
+
+    }
+}
---
+++ TweetApp/Migrations/20210401132132_TweetAppDB.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TweetApp.Migrations
+{
+    public partial class TweetAppDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    EmailId = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    Gender = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    DOB = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Password = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tweet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    Tweeets = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tweet", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Tweet__UserId__1A14E395",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tweet_UserId",
+                table: "Tweet",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tweet");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
